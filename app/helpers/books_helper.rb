ee91ae056ea5dde19e3487434f1a685ede712c9b
Jen Dobson <jendobson@gmail.com>
2016-01-22 08:03:01 -0800

    first commit

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,5 +1,4 @@
 module BooksHelper
-    # Returns the Favorites icon for the given song and user.
   def favorites_link_for(reader, book)
     if reader && Favorite.exists?(book_id: book.id, reader_id: reader.id)
       link_to("Remove From Favorites", Favorite.find_by(book_id: book.id, reader_id: reader.id), :method => 'delete', remote: true)
