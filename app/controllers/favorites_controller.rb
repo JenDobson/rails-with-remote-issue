a2bb619b356ec274b1e5740d0d03ff9e5814bda2
Jen Dobson <jendobson@gmail.com>
2016-01-24 11:58:34 -0800

    Fixed JS deleting book issue

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,22 @@
 class FavoritesController < ApplicationController
 def create
-    reader = Reader.find(params[:reader_id])
+    @reader = Reader.find(params[:reader_id])
     @book = Book.find(params[:book_id])
-    reader.add_favorite(Book.find(params[:book_id]))
-    redirect_to @book
+    @reader.add_favorite(Book.find(params[:book_id]))
+    respond_to do |format|
+      format.html { redirect_to @book }
+      format.js
+    end
   end
 
   def destroy
     fav = Favorite.find(params[:id])
     @book = Book.find(fav.book_id)
+    @reader = Reader.find(fav.reader_id)
     fav.destroy
-    redirect_to @book
+    respond_to do |format|
+      format.html { redirect_to @book }
+      format.js
+    end
   end
 end
--- app/helpers/favorites_helper.rb
+++ app/helpers/favorites_helper.rb
@@ -1,10 +1,10 @@
 module FavoritesHelper
    def favorites_link_for(reader, book)
     if reader && Favorite.exists?(book_id: book.id, reader_id: reader.id)
-      link_to("Remove From Favorites", Favorite.find_by(book_id: book.id, reader_id: reader.id), :method => 'delete', remote: true)
+      link_to("Remove From Favorites", favorite_path(Favorite.find_by(book_id: book.id, reader_id: reader.id)), :method => 'delete', remote: true, id: "add_remove_favorites_link")
                                   
     elsif reader && !Favorite.exists?(book_id: book.id, reader_id: reader.id)
-      link_to("Add To Favorites", favorites_path(book_id: book.id, reader_id: reader.id), :method => 'post', remote: true)
+      link_to("Add To Favorites", favorites_path(book_id: book.id, reader_id: reader.id), :method => 'post', remote: true,  id: "add_remove_favorites_link")
     end
   end
 end
